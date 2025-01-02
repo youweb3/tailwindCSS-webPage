f76ade792404087796309a3e9a902fbd978b3b34
Yousef Shaikhan <105981004+youweb3@users.noreply.github.com>
2025-01-02 02:22:18 +0000

    initial-commit
    made a tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors:{
+      transparent: 'transparent',
+      white: '#ffffff',
+      black: '#000000',
+      tahiti: '#3ab7bf',
+    },
+
+    extend: {},
+  },
+  plugins: [],
+}
+
